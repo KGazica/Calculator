04d6c02f59f4d4bef9702a36f9d8ab6656c3eafb
KGazica <kresimir.gazica@gmail.com>
2022-07-26 21:50:31 +0200

    js finished

--- script.js
+++ script.js
@@ -29,3 +29,57 @@ function getFormattedNumber(num) {
   var value = n.toLocaleString("en");
   return value;
 }
+
+//clear comas in output
+
+function reverseNumberFormat(num) {
+  return Number(num.replace(/,/g, ""));
+}
+
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function () {
+    if (this.id == "clear") {
+      printPast("");
+      printOutput("");
+    } else if (this.id == "backspace") {
+      var output = reverseNumberFormat(getOutput()).toString();
+      if (output) {
+        output = output.substr(0, output.length - 1);
+        printOutput(output);
+      }
+    } else {
+      var output = getOutput();
+      var past = getPast();
+      if (output == "" && past != "") {
+        if (isNaN(past[past.length - 1])) {
+          past = past.substr(0, past.length - 1);
+        }
+      }
+      if (output != "" || past != "") {
+        output = output == "" ? output : reverseNumberFormat(output);
+        past = past + output;
+        if (this.id == "=") {
+          var result = eval(past);
+          printOutput(result);
+          printPast("");
+        } else {
+          past = past + this.id;
+          printPast(past);
+          printOutput("");
+        }
+      }
+    }
+  });
+}
+
+var number = document.getElementsByClassName("number");
+for (var i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    var output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      output = output + this.id;
+      printOutput(output);
+    }
+  });
+}
